7a2f2984ba24641925c781f2333938d748a82273
spoorthysparx <spporthysparx@gmail.com>
2022-04-05 12:02:47 +0530

    added html files for each function

--- main.py
+++ main.py
@@ -9,9 +9,17 @@ bootstrap = Bootstrap5(app)
 def hello_world():
    return render_template('home.html')
 
-@app.route('/maskingRules')
+@app.route('/maskingDataGen')
+def masking_rules_gen():
+   return render_template("masking_data_generator/data_gen.html")
+
+@app.route('/maskingRulesGen')
 def masking_rules_gen():
    return render_template("masking_rules_config_generator/rules_gen.html")
+
+@app.route('/columnGen')
+def masking_rules_gen():
+   return render_template("pii_column_generator/columns_gen.html")
 
 if __name__ == '__main__':
    app.run(debug=True)
